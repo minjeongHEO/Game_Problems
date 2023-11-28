2c24743c9885d5baecc187311729f90b8b046a9c
minjeongHEO <hmjoung9770@gmail.com>
2023-11-28 20:55:53 +0900

    WIP: 객체를 이용하여 데이터 저장하기 중

--- Sokoban/step01.js
+++ Sokoban/step01.js
@@ -10,6 +10,15 @@ function readLine(inputValue) {
   console.log(textArr);
 }
 
+// (2) 데이터 저장하기
+// 위 값을 읽고 저장할 수 있는 적당한 객체 (혹은 클래스)를 생성하고 문자열로부터 읽은 값을 변환해서 저장한다.
+class Savemap {
+  constructor(text) {
+    this.map = text;
+  }
+}
+let map = new Savemap(textArr);
+
 document.getElementById('reg-btn').addEventListener('click', function (e) {
   const inputValue = document.getElementById('text-area').value;
   if (inputValue == '' || inputValue === null || inputValue === undefined) {
